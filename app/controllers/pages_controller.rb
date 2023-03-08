51648eeafa0fd6de826f6b7c8005f643e039cd92
Riwan William Bououli <riwan.b@live.fr>
2023-03-08 17:56:33 +0100

    home changed, nav bar more or less fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @orders = Order.first(3)
-    @restaurants = Restaurant.all
   end
 
   def choose_restaurant
+    @orders = Order.first(3)
+    @restaurants = Restaurant.all
   end
 end
