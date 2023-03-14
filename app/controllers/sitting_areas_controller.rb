048aad4136f06f5047080adbf2132badf73c941e
benoit lebon <ben142007@live.fr>
2023-03-14 16:53:23 +0100

    colapsus

--- app/controllers/sitting_areas_controller.rb
+++ app/controllers/sitting_areas_controller.rb
@@ -4,5 +4,6 @@ class SittingAreasController < ApplicationController
     @restaurant = @sitting_area.restaurant
     @pending_orders = @sitting_area.orders.where(status: "en attente")
     @passed_orders = @sitting_area.orders - @pending_orders
+    @orders = Order.all
   end
 end
