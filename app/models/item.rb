2b067522f0ba61a231865ac65e78601f25d5fccc
Thomas Kamoun <thomas-kamoun@hotmail.fr>
2023-03-16 11:46:18 +0100

    dropDown pluralize better service OK

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
+require 'active_support'
+
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :restaurant, optional: true
   has_many :orders
+
+  def plural_name
+    name.pluralize
+  end
 end
